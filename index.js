95c06063d827d87cc9fbda4170d3d2dd08216026
sam <sam@freighttrust.com>
2021-03-31 20:35:54 -0700

    refactor(eslint): refactor plugin system and rules

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-// @file config-export
-// @license Apache-2.0
+/**
+ * @file eslintrc.js
+ * @summary web3 eslint configuration
+ * @version 1.0.0
+ * @license MIT
+ */
 'use strict';
+
+const eslintrc = require('./.eslintrc');
 
 module.exports = {
   extends: [require.resolve('./core'), require.resolve('./plugins/react')],
 };
+/** @exports eslintrc **/
---
+++ rules/best-practices.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    // @see https://eslint.org/docs/rules/curly
+    curly: 'error',
+  },
+};
